5565d0c55550e0fc29053a00895d07cf6b528fa6
Seongmin Mun <51292738+seongmin-mun@users.noreply.github.com>
2019-11-12 00:49:36 +0100

    Add files via upload

---
+++ Java/한글 로마나이제이션/MainRun.java
@@ -0,0 +1,12 @@
+package Main;
+
+import Processing.Visualization.Romanization;
+
+public class MainRun {
+    public static void main (String [] args) throws Exception {
+        
+        String out = Romanization.yale_romanization("초성만 입력 했을 시엔 초성은 무시해서 List에 추가합니다.");
+        System.out.println(out);
+
+    }
+}
